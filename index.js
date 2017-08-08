34cc8684b56b191582584af35d5d96c92a90454a
Marcus Pop <pop_marcus_98@yahoo.com>
2017-08-08 14:22:54 +0000

    ..

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const VERIFY_TOKEN = 'what_code_do_i_need_afterall_oh_my_verify_me_please';
 
+const url = require('url');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -21,8 +23,10 @@ app.get('/', (request, response) => {
 });
 
 app.get('/webhook/', (request, response) => {
-  if (request.query['hub.verify_token'] === VERIFY_TOKEN)
-    request.status(200).send(request.query['hub.challenge']);
+  const query = url.parse(request.url, true).query;
+  
+  if (query['hub.verify_token'] === VERIFY_TOKEN)
+    request.status(200).send(query['hub.challenge']);
   response.status(403).send('Forbidden');          
 });
 
