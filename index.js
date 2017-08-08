c25c5038b94aa25da19900dda98713a499ab437a
Marcus Pop <pop_marcus_98@yahoo.com>
2017-08-08 15:25:08 +0000

    change is good

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const VERIFY_TOKEN = 'what_code_do_i_need_afterall_oh_my_verify_me_please';
+const PAGE_TOKEN = 'EAAaezGvfcCwBAKxugZCpzz2zjd6bnA2DGUXGZAfX6ZBF1zr2Swd4urTMjbAweUtJHRj0Vy3zXz5AdnPAS8dR1U66Gx21bJuYI9kO7mXVhIMyykXRiodxRj4KhZAZBjooTFD25utXYgNsEEwSKjUavNFFtvW09fOVPYqVTG9xmWAZDZD';
 
 const url = require('url');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const req = require('request');
 
 const app = express();
 
@@ -15,6 +17,33 @@ app.set('case sensitive routing', true);
 app.use(bodyParser.urlencoded({ 'extended': true}));		
 app.use(bodyParser.json());
 
+function processMessage(event) {
+  sendMessage(event.sender.id, event.message);
+}
+
+function sendMessage(id, text) {
+  callSendApi({
+    recipient: { id },
+    message: { text }
+  });
+}
+
+function callSendApi(data) {
+  req({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: PAGE_TOKEN},
+    method: 'POST',
+    json: data
+  }, (error, response, body) => {
+    if (!error && response.status === 200) {
+      console.log('Succesfully sent message to user');
+    }
+    else {
+      console.log('Error: ', error);
+    }
+  });
+}
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -22,12 +51,29 @@ app.get('/', (request, response) => {
   response.status(200).send('index');
 });
 
-app.get('/webhook/', (request, response) => {
+// verify page if valid
+app.get('/webhook', (request, response) => {
   const query = url.parse(request.url, true).query;
   
   if (query['hub.verify_token'] === VERIFY_TOKEN)
     response.status(200).send(query['hub.challenge']);
   response.status(403).send('Forbidden');          
+});
+
+// receive message
+app.post('/webhook', (request, response) => {
+  if (request.body.object === 'page') {
+    request.body.forEach((entry) => {
+      entry.messaging.forEach((event) => {
+        if (event.message) {
+          processMessage(event);
+        }
+        else {
+          console.log('Uhm, you were supposed to send me a message ):');
+        }
+      });;
+    });
+  }
 });
 
 app.get('*', (request, response) => {
