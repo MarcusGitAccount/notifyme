5b928862ffcdc4508f27dd35da4f9d7527ad468a
Marcus Pop <pop_marcus_98@yahoo.com>
2017-08-08 21:41:07 +0000

    change is good

--- Singleton.js
+++ Singleton.js
@@ -8,7 +8,7 @@ const _db = Symbol('_db');
 class MongoConnection {
   constructor() {
     console.log('Connected to mongodb.');
-    mongoose.connect('mongodb://marcuspop:dbconnection_pass~eurobtc2017@ds039271.mlab.com:39271/notifymedb');
+    mongoose.connectUri('mongodb://marcuspop:dbconnection_pass~eurobtc2017@ds039271.mlab.com:39271/notifymedb');
     mongoose.Promise = global.Promise;
     this[_db] = mongoose.connection;
   }
