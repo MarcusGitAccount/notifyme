8a6d990be078c39f66dfb406d84864e530a94fa4
Marcus Pop <pop_marcus_98@yahoo.com>
2017-08-09 06:59:12 +0000

    change is good

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const messages = {
     console.log('CURRENCY');
     
     if (currenciesRate[message] === {} || !currenciesRate[message]) {
-      callback(`Couldn't retrieve currency. Try later` + currenciesRate[message].last);
+      callback(`Couldn't retrieve currency. Try later` + message);
       return ;
     }
     callback(`1 ${message} is worth ${currenciesRate[message].last} bitcoin`);
